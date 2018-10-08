952cb6d3263a5e7f9e7fcbdf33fd350a9cb1dbd3
Hank Tom <hanktom@gmail.com>
2018-10-09 03:20:56 +0800

    if...else, extract method

--- src/main/kotlin/com/tom/Student.kt
+++ src/main/kotlin/com/tom/Student.kt
@@ -3,6 +3,42 @@ package com.kotlin
 import java.util.*
 
 fun main(args: Array<String>) {
+//    userInput()
+    val stu = Student("Hank", 77, 99)
+    stu.print()
+    val test = 123
+    println("Test is : $test")
+    println("High score: ${stu.highest()}")
+}
+
+class Student(var name: String?, var english: Int, var math: Int) {
+    fun print() {
+        println(name + "\t" + english + "\t" + math +
+                "\t" + (english+math)/2)
+    }
+
+    fun highest() : Int {
+        var max = if (english > math) {
+            println("english")
+            english
+        } else {
+            println("math")
+            math
+        }
+        /*if (english > math) {
+            max = english
+        }else {
+            max = math
+        }*/
+        return max
+    }
+
+    fun nameCheck() {
+        println(name?.length)
+    }
+}
+
+private fun userInput() {
     val scanner = Scanner(System.`in`)
     print("Please enter student's name: ")
 //    var name = scanner.next()
@@ -16,13 +52,3 @@ fun main(args: Array<String>) {
     stu.nameCheck()
 }
 
-class Student(var name: String?, var english: Int, var math: Int) {
-    fun print() {
-        println(name + "\t" + english + "\t" + math +
-                "\t" + (english+math)/2)
-    }
-
-    fun nameCheck() {
-        println(name?.length)
-    }
-}
